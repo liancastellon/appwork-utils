2a33d5896b19dc83b9e80bda638bd4c3806ba543
daniel <daniel@21714237-3853-44ef-a1f0-ef8f03a7d1fe>
2019-05-29 16:02:07 +0000

    SingleAppInstance: -refactored daemon to handle each client in own thread. old handling could block client messages and lead to timeout

--- src/org/appwork/utils/StringUtils.java
+++ src/org/appwork/utils/StringUtils.java
@@ -95,64 +95,64 @@ public class StringUtils {
      * taken from http://stackoverflow.com/questions/4731055/whitespace-matching-regex-java
      */
     final private static String whitespace_chars = "[" /*
-     * dummy empty string for homogeneity
-     */
-            + "\\u0009" // CHARACTER
-            // TABULATION
-            + "\\u000A" // LINE
-            // FEED
-            // (LF)
-            + "\\u000B" // LINE
-            // TABULATION
-            + "\\u000C" // FORM
-            // FEED
-            // (FF)
-            + "\\u000D" // CARRIAGE
-            // RETURN
-            // (CR)
-            + "\\u0020" // SPACE
-            + "\\u0085" // NEXT
-            // LINE
-            // (NEL)
-            + "\\u00A0" // NO-BREAK
-            // SPACE
-            + "\\u1680" // OGHAM
-            // SPACE
-            // MARK
-            + "\\u180E" // MONGOLIAN
-            // VOWEL
-            // SEPARATOR
-            + "\\u2000" // EN QUAD
-            + "\\u2001" // EM QUAD
-            + "\\u2002" // EN SPACE
-            + "\\u2003" // EM SPACE
-            + "\\u2004" // THREE-PER-EM
-            // SPACE
-            + "\\u2005" // FOUR-PER-EM
-            // SPACE
-            + "\\u2006" // SIX-PER-EM
-            // SPACE
-            + "\\u2007" // FIGURE
-            // SPACE
-            + "\\u2008" // PUNCTUATION
-            // SPACE
-            + "\\u2009" // THIN
-            // SPACE
-            + "\\u200A" // HAIR
-            // SPACE
-            + "\\u2028" // LINE
-            // SEPARATOR
-            + "\\u2029" // PARAGRAPH
-            // SEPARATOR
-            + "\\u202F" // NARROW
-            // NO-BREAK
-            // SPACE
-            + "\\u205F" // MEDIUM
-            // MATHEMATICAL
-            // SPACE
-            + "\\u3000" // IDEOGRAPHIC
-            // SPACE
-            + "]";
+                                                        * dummy empty string for homogeneity
+                                                        */
+                                                         + "\\u0009" // CHARACTER
+                                                         // TABULATION
+                                                         + "\\u000A" // LINE
+                                                         // FEED
+                                                         // (LF)
+                                                         + "\\u000B" // LINE
+                                                         // TABULATION
+                                                         + "\\u000C" // FORM
+                                                         // FEED
+                                                         // (FF)
+                                                         + "\\u000D" // CARRIAGE
+                                                         // RETURN
+                                                         // (CR)
+                                                         + "\\u0020" // SPACE
+                                                         + "\\u0085" // NEXT
+                                                         // LINE
+                                                         // (NEL)
+                                                         + "\\u00A0" // NO-BREAK
+                                                         // SPACE
+                                                         + "\\u1680" // OGHAM
+                                                         // SPACE
+                                                         // MARK
+                                                         + "\\u180E" // MONGOLIAN
+                                                         // VOWEL
+                                                         // SEPARATOR
+                                                         + "\\u2000" // EN QUAD
+                                                         + "\\u2001" // EM QUAD
+                                                         + "\\u2002" // EN SPACE
+                                                         + "\\u2003" // EM SPACE
+                                                         + "\\u2004" // THREE-PER-EM
+                                                         // SPACE
+                                                         + "\\u2005" // FOUR-PER-EM
+                                                         // SPACE
+                                                         + "\\u2006" // SIX-PER-EM
+                                                         // SPACE
+                                                         + "\\u2007" // FIGURE
+                                                         // SPACE
+                                                         + "\\u2008" // PUNCTUATION
+                                                         // SPACE
+                                                         + "\\u2009" // THIN
+                                                         // SPACE
+                                                         + "\\u200A" // HAIR
+                                                         // SPACE
+                                                         + "\\u2028" // LINE
+                                                         // SEPARATOR
+                                                         + "\\u2029" // PARAGRAPH
+                                                         // SEPARATOR
+                                                         + "\\u202F" // NARROW
+                                                         // NO-BREAK
+                                                         // SPACE
+                                                         + "\\u205F" // MEDIUM
+                                                         // MATHEMATICAL
+                                                         // SPACE
+                                                         + "\\u3000" // IDEOGRAPHIC
+                                                         // SPACE
+                                                         + "]";
 
     public static String trim(String input) {
         if (input != null) {
@@ -202,14 +202,13 @@ public class StringUtils {
     public static boolean equals(final String x, final String y) {
         if (x == y) {
             return true;
-        }
-        if (x == null && y != null) {
+        } else if (x == null && y != null) {
             return false;
-        }
-        if (y == null && x != null) {
+        } else if (y == null && x != null) {
             return false;
+        } else {
+            return x.equals(y);
         }
-        return x.equals(y);
     }
 
     /**
@@ -220,11 +219,9 @@ public class StringUtils {
     public static boolean equalsIgnoreCase(final String pass, final String pass2) {
         if (pass == pass2) {
             return true;
+        } else {
+            return pass != null && pass.equalsIgnoreCase(pass2);
         }
-        if (pass == null && pass2 != null) {
-            return false;
-        }
-        return pass.equalsIgnoreCase(pass2);
     }
 
     private static String EMPTY_SPACE_STRING = "                                                                                                                                                                                                                                                             ";
